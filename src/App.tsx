85b2aa708fc3ee82b2e3d4aa99e0eafd14aa990e
unnamed <>
2020-03-05 14:48:52 +0100

    Add styles to Sign form

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,35 @@ import React from 'react';
 import 'typeface-roboto';
 import './App.css';
 import SignIn from './containers/Auth/SignIn/SignIn';
-import { Container } from '@material-ui/core';
+import {
+	ThemeProvider,
+	createMuiTheme,
+	styled
+} from '@material-ui/core';
+import * as colors from '@material-ui/core/colors/';
+
+const theme = createMuiTheme({
+	palette: {
+		primary: colors.teal,
+		secondary: colors.orange
+	},
+	typography: {
+		fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif'
+	}
+});
+
+const StyledApp = styled('div')({
+	background: '#F8F8F8',
+	minHeight: '100vh'
+});
 
 function App() {
 	return (
-		<div className="App">
-			<Container
-				maxWidth="lg"
-				style={{
-					background: '#eee',
-				}}
-			>
+		<ThemeProvider theme={theme}>
+			<StyledApp className="App" id="AppRootComponent">
 				<SignIn />
-			</Container>
-		</div>
+			</StyledApp>
+		</ThemeProvider>
 	);
 }
 
--- src/containers/Auth/SignIn/SignIn.tsx
+++ src/containers/Auth/SignIn/SignIn.tsx
@@ -5,66 +5,177 @@ import {
 	Grid,
 	Avatar,
 	Paper,
-	Container
+	Box,
+	makeStyles,
+	Button,
+	Container,
+	Theme,
+	Link,
+	IconButton,
+	Slide
 } from '@material-ui/core';
-import classes from './SignIn.module.css';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+
+const useStyle = makeStyles((theme: Theme) => ({
+	container: {
+		height: '100vh',
+
+		display: 'flex',
+		justifyContent: 'stretch',
+		alignItems: 'center'
+	},
+	paper: {
+		width: '100%',
+		padding: 30
+	},
+	header: {
+		paddingBottom: theme.spacing(2)
+	},
+	avatarBox: {
+		position: 'relative',
+		width: theme.spacing(12),
+		height: theme.spacing(12)
+	},
+	avatar: {
+		width: '100%',
+		height: '100%'
+	},
+	avatarCamera: {
+		position: 'absolute',
+		bottom: -10,
+		right: -10,
+		background: 'white'
+	}
+}));
 
 const SignIn = () => {
-	const [name, setName] = useState('initialState');
+	const classes = useStyle();
+
+	const [avatarUrl, setAvatarUrl] = useState(
+		'https://lh3.googleusercontent.com/-_XU_M6CM4qI/AAAAAAAAAAI/AAAAAAAAAAA/AKF05nA0jBNgRe6Wq9N0M6Ebhz71nSfTWQ/s48-c/photo.jpg'
+	); /* to do: ability to take a picture from laptop camera read more at : http://www.danielmayor.com/take-photo-with-html5-javascript */
+	const [name, setName] = useState('');
 	const [emailAddress, setEmailAddress] = useState('test@test.com');
 	const [password, setPassword] = useState('');
 	const [passwordConfirm, setPasswordConfirm] = useState('');
+	const [isSignIn, setIsSignIn] = useState(false);
 
 	return (
-		<Container maxWidth="sm">
-			<Paper>
-				<Typography variant="h3" align="center">
-					Sign Up
-				</Typography>
-				<Grid
-					container
-					direction="column"
-					alignItems="center"
-					justify="center"
-					spacing={3}
-				>
-					<Avatar
-						alt="user avatar"
-						src="https://lh3.googleusercontent.com/-_XU_M6CM4qI/AAAAAAAAAAI/AAAAAAAAAAA/AKF05nA0jBNgRe6Wq9N0M6Ebhz71nSfTWQ/s48-c/photo.jpg"
-					/>
+		<Container maxWidth="sm" className={classes.container}>
+			<Paper className={classes.paper} elevation={5}>
+				<Box className={classes.header}>
+					<Typography variant="h3" align="center">
+						Sign Up
+					</Typography>
+				</Box>
+				<form>
+					<Grid
+						container
+						direction="column"
+						alignItems="stretch"
+						justify="center"
+						spacing={3}
+					>
+						<Box justifyContent="center" display="flex">
+							<Box
+								className={classes.avatarBox}
+								display="flex"
+								alignItems="center"
+								justifyContent="center"
+							>
+								<Avatar
+									alt="user avatar"
+									src={avatarUrl}
+									className={classes.avatar}
+								/>
+								<IconButton
+									className={classes.avatarCamera}
+									color="primary"
+									aria-label="upload picture"
+									component="span"
+								>
+									<PhotoCamera />
+								</IconButton>
+							</Box>
+						</Box>
+						<Slide
+							direction="left"
+							in={isSignIn}
+							mountOnEnter
+							unmountOnExit
+						>
+							<Grid item>
+								<TextField
+									fullWidth
+									variant="outlined"
+									label="Name"
+									autoFocus
+									required
+									value={name}
+								/>
+							</Grid>
+						</Slide>
+						<Grid item>
+							<TextField
+								fullWidth
+								variant="outlined"
+								label="Email Address"
+								type="email"
+								required
+								value={emailAddress}
+							/>
+						</Grid>
+						<Grid item>
+							<TextField
+								fullWidth
+								variant="outlined"
+								label="Password"
+								value={password}
+								required
+								type="password"
+							/>
+						</Grid>
+						<Slide
+							direction="left"
+							in={isSignIn}
+							mountOnEnter
+							unmountOnExit
+						>
+							<Grid item>
+								<TextField
+									variant="outlined"
+									fullWidth
+									label="Confirm Password"
+									value={passwordConfirm}
+									required
+									type="password"
+								/>
+							</Grid>
+						</Slide>
+						<Grid item>
+							<Link
+								onClick={() =>
+									setIsSignIn(prevState => !prevState)
+								}
+								variant="body2"
+								style={{ cursor: 'pointer' }}
+							>
+								Already have an account? Sign in
+							</Link>
+						</Grid>
 
-					<Grid item>
-						<TextField
-							variant="outlined"
-							label="Name"
-							value={name}
-						/>
+						<Grid item>
+							<Button
+								fullWidth
+								variant="contained"
+								color="primary"
+								type="submit"
+							>
+								Sign Up
+							</Button>
+						</Grid>
 					</Grid>
-					<Grid item lg={7}>
-						<TextField
-							fullWidth
-							variant="outlined"
-							label="Email Address"
-							value={emailAddress}
-						/>
-					</Grid>
-					<Grid item>
-						<TextField
-							variant="outlined"
-							label="Password"
-							value={password}
-							type="password"
-						/>
-					</Grid>
-					<Grid item>
-						<TextField
-							variant="outlined"
-							label="Confirm Password"
-							value={passwordConfirm}
-							type="password"
-						/>
-					</Grid>
-				</Grid>
+				</form>
 			</Paper>
 		</Container>
 	);
