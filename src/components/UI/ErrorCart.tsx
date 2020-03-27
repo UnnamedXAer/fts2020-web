7027c1c0ca82fcbc135eaf96b1a4b9c74afb1750
unnamed <>
2020-03-27 12:30:44 +0100

    Improve ErrorCart component

--- src/components/UI/ErrorCart.tsx
+++ src/components/UI/ErrorCart.tsx
@@ -1,31 +1,44 @@
 import React from 'react';
-import { Paper, Typography, Box } from '@material-ui/core';
-import { ErrorRounded as ErrorRoundedIcon} from '@material-ui/icons';
+import { Paper, Typography, Box, makeStyles } from '@material-ui/core';
+import { ErrorOutlineRounded as ErrorRoundedIcon } from '@material-ui/icons';
 
 interface Props {
 	message: string | null;
+	headerMessage?: string;
 	showHeader?: boolean;
+	iconSize?: 'inherit' | 'default' | 'large' | 'small' | undefined;
 }
 
-const ErrorCart: React.FC<Props> = ({ message, showHeader }) => {
-	return (
-		message !== null ? (
-			<Paper elevation={4} color="error">
-				<Box display="flex" flexDirection="row">
-					<Box>
-						{showHeader && (
-							<Typography variant="h6">
-								Opss, something went wrong
-							</Typography>
-						)}
-						<Typography variant="body1">{message}</Typography>
-					</Box>
-					<ErrorRoundedIcon color="error"  />
+const ErrorCart: React.FC<Props> = ({
+	message,
+	headerMessage = 'An error occurred',
+	showHeader,
+	iconSize = 'default'
+}) => {
+	const classes = useStyle();
+	return message !== null ? (
+		<Paper className={classes.cart} elevation={4} color="error">
+			<Box
+				display="flex"
+				flexDirection="row"
+				justifyContent="space-between"
+			>
+				<Box>
+					{showHeader && (
+						<Typography variant="h6">{headerMessage}</Typography>
+					)}
+					<Typography variant="body1">{message}</Typography>
 				</Box>
-			</Paper>
-		)
-		: null
-	);
+				<ErrorRoundedIcon color="error" fontSize={iconSize} />
+			</Box>
+		</Paper>
+	) : null;
 };
+
+const useStyle = makeStyles({
+	cart: {
+		padding: 16
+	}
+});
 
 export default ErrorCart;
